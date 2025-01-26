51ad82eecb15f2c12dd767a7b5f95a452e2e3903
Admin <ariantajbakh@gmail.com>
2025-01-26 01:12:24 -0500

    re

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mysite.wsgi 
+web: daphne mysite.asgi:application -b 0.0.0.0 -p $PORT 
--- mysite/asgi.py
+++ mysite/asgi.py
@@ -11,8 +11,11 @@ from myapp.consumers import OrderConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
+# Initialize Django ASGI application early
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter([
             re_path(r'ws/orders/$', OrderConsumer.as_asgi()),
