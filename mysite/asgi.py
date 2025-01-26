34cb2d23f0223bad643146efe245d3f461706e14
Admin <ariantajbakh@gmail.com>
2025-01-26 01:14:50 -0500

    test

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -3,13 +3,16 @@ ASGI config for mysite project.
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()  # Set up Django before importing consumers
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
 from myapp.consumers import OrderConsumer
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 # Initialize Django ASGI application early
 django_asgi_app = get_asgi_application()
